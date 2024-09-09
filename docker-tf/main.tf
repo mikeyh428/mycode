02d40c2759af03bb2b8a0c1e1965ee3ced95b101
mikeyh428 <mikeh@aslanius.net>
2024-09-09 19:54:51 +0000

    terraform and docker

---
+++ docker-tf/main.tf
@@ -0,0 +1,29 @@
+#My name is @#$@%@$
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+#changed to 2.25.0 from 2.22.0
+      version = "~> 2.25.0"
+    }
+  }
+}
+
+provider "docker" {}
+
+#old version
+resource "docker_image" "nginx" {
+  name         = "nginx:1.19.6"
+  keep_locally = true    // keep image after "destroy"
+}
+
+#sucky
+resource "docker_container" "nginx" {
+  image = docker_image.nginx.image_id
+  name  = "tutorial"
+  ports {
+    internal = 80
+    external = 2224
+  }
+}
+
